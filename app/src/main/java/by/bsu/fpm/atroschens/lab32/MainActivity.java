7280889c295dec645f35b1f8ae105a2fbef93a63
nikita <nikitatroshenko@gmail.com>
2016-11-21 22:55:04 +0300

    Initial commit

---
+++ app/src/main/java/by/bsu/fpm/atroschens/lab32/MainActivity.java
@@ -0,0 +1,13 @@
+package by.bsu.fpm.atroschens.lab32;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
