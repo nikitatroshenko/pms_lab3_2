af1159373e35a6d18ea4007ef018f2377d05e26b
nikita <nikitatroshenko@gmail.com>
2016-12-05 14:16:34 +0300

    Working prototype

--- app/src/main/java/by/bsu/fpm/atroschens/lab32/MainActivity.java
+++ app/src/main/java/by/bsu/fpm/atroschens/lab32/MainActivity.java
@@ -1,13 +1,33 @@
 package by.bsu.fpm.atroschens.lab32;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editUrl = (EditText) findViewById(R.id.edit_url);
+    }
+
+
+    public void onGoClick(View view) {
+        try {
+            String text = editUrl.getText().toString();
+            Uri uri = Uri.parse(text);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } catch (Exception ex) {
+            editUrl.setError(ex.getLocalizedMessage());
+        }
     }
 }
